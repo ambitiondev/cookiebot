ccf9ef4ad51e2b10c3776f5f98a32613bd3222cf
Berrie Nachtweh <info@ambitiondev.io>
2023-11-15 14:49:38 +0100

    chore(sandbox): add route links

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+	// Vendor
+	import { useCookiebot } from '@ambitiondev/vue-cookiebot';
+	import { onMounted } from 'vue';
+	import { useI18n } from 'vue-i18n';
+
+	// Composable
+	const { locale } = useI18n();
+	const { consentBanner } = useCookiebot(import.meta.env.VITE_COOKIEBOT_ID, {
+		culture: locale.value,
+	});
+
+	onMounted(async () => {
+		await consentBanner();
+	});
+</script>
+
 <template>
+	<router-link to="/">Home</router-link>
+	<router-link to="/consentpage">Consent page</router-link>
 	<router-view></router-view>
 </template>
--- sandbox/src/views/HomePage.vue
+++ sandbox/src/views/HomePage.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
 	// Vendor
-	import { useCookiebot } from '@ambitiondev/vue-cookiebot';
-	import { onMounted } from 'vue';
 	import { useI18n } from 'vue-i18n';
 
 	// Composable
 	const { t, locale } = useI18n();
-	const { consentBanner } = useCookiebot(import.meta.env.VITE_COOKIEBOT_ID, {
-		culture: locale.value,
-	});
-
-	onMounted(async () => {
-		await consentBanner();
-	});
 </script>
 
 <template>
