6bd9b73fa5636e963de0c62a2a9fbbc17664e829
Berrie Nachtweh <info@ambitiondev.io>
2024-02-21 08:24:26 +0100

    fix(vue): run scripts after route change

--- packages/vue/src/plugin.ts
+++ packages/vue/src/plugin.ts
@@ -7,6 +7,18 @@ import type { PluginOptions } from '@ambitiondev/cookiebot-common';
 const cookieBot = {
 	install: (app: App, options: PluginOptions) => {
 		app.provide('cookieBotOptions', options);
+
+		requestAnimationFrame(() => {
+			if (typeof app.config.globalProperties.$router === 'object') {
+				app.config.globalProperties.$router.afterEach(() => {
+					if ('Cookiebot' in window) {
+						requestAnimationFrame(() => {
+							window.Cookiebot.runScripts();
+						});
+					}
+				});
+			}
+		});
 	},
 };
 
