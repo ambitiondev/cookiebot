c2141d25f09894b043b492a135bc559141f5a639
Berrie Nachtweh <info@ambitiondev.io>
2024-05-30 10:24:44 +0200

    style(nuxt): fixture module type

--- packages/nuxt/tests/fixtures/cookiebot/nuxt.config.ts
+++ packages/nuxt/tests/fixtures/cookiebot/nuxt.config.ts
@@ -1,7 +1,8 @@
+import type { NuxtModule } from '@nuxt/schema';
 import MyModule from '../../../src/module';
 
 export default defineNuxtConfig({
-    modules: [MyModule as any],
+    modules: [MyModule as unknown as NuxtModule<Record<string, unknown>>],
     cookiebot: {
         cookieBotId: import.meta.env.COOKIEBOT_ID,
     },
