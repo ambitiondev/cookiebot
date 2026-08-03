b98b33663c79dce8a2236ff4509869e7ca91c974
Berrie Nachtweh <berrie.nachtweh@nobears.nl>
2026-08-03 13:45:45 +0200

    ci(many): test script

--- packages/vue/vitest.config.ts
+++ packages/vue/vitest.config.ts
@@ -2,7 +2,7 @@ import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  envDir: ".",
+  envDir: "./sandbox",
   plugins: [vue()],
   test: {
     environment: "jsdom",
