f331157a4d154a438702662fc83866dbb3a93c5e
Berrie Nachtweh <berrie.nachtweh@nobears.nl>
2026-08-03 15:57:09 +0200

    test(nuxt): resolve alias in fixture apps

--- packages/nuxt/test/e2e/fixtures/runtime-config/nuxt.config.ts
+++ packages/nuxt/test/e2e/fixtures/runtime-config/nuxt.config.ts
@@ -1,6 +1,13 @@
+import { fileURLToPath } from "node:url";
+
 import CookiebotModule from "../../../../src/module";
 
 export default defineNuxtConfig({
+  alias: {
+    "@ambitiondev/cookiebot-common": fileURLToPath(
+      new URL("../../../../../common/src/index.ts", import.meta.url),
+    ),
+  },
   modules: [CookiebotModule],
   cookiebot: {
     useRuntimeConfig: true,
