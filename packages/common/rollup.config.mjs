43dc68ff6550adbd5bec23505b927bb2dce8e771
Berrie Nachtweh <info@ambitiondev.io>
2026-08-04 09:14:36 +0200

    build(common): bundle esm version

---
+++ packages/common/rollup.config.mjs
@@ -0,0 +1,22 @@
+const outputConfigs = {
+  esm: {
+    dir: "dist/",
+    entryFileNames: "[name].esm.js",
+    chunkFileNames: "[name]-[hash].esm.js",
+    format: "es",
+    sourcemap: true,
+  },
+};
+
+const createConfig = (_, output) => {
+  return {
+    input: "dist-transpiled/index.js",
+    output,
+  };
+};
+
+const configs = Object.keys(outputConfigs).map((format) =>
+  createConfig(format, outputConfigs[format]),
+);
+
+export default configs;
